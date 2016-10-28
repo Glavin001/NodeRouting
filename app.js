1ab7722f12d50d5bf1b2e9f421a3b78468668a41
Arsum Iqbal <arsum04@Arsums-MacBook-Air.local>
2016-10-28 19:00:32 -0300

    Created chainable route handler

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello World!");
-});
+//chained route handler to minimize redundancy
+app.route("/megaHelloWorld")
+  .get(function(req,res ) {
+    res.send("This was a get request");
+  })
+  .post(function(req,res) {
+    res.send("This was a post request");
+  })
+  .put(function(req, res) {
+    res.send("This was a put request");
+  })
+  .delete(function(req,res) {
+    res.send("This was a delete request");
+  });
 
 app.listen(3000, function () {
   console.log("Listening on port 3000");
